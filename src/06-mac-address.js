0c96c328744a793e2d56cddbb909c66c03d9ab08
ckachok <mefelion89@gmail.com>
2021-04-10 02:34:03 +0300

    feat: add a solution for the 'mac address' task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,9 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const arrNumMacAddress = n.split('-');
+  return arrNumMacAddress.every((elem) => (elem.length === 2 && /[A-F0-9]{2}/g.test(elem)));
 }
 
 module.exports = isMAC48Address;
